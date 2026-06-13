7bcd8c8e314a2c6f188da100c84d7c469321ae71
Allan <allanpereira773@gmail.com>
2026-06-13 15:19:14 -0300

    Return product image

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,6 +8,7 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
     public OrderItemDTO(){
 
@@ -15,11 +16,12 @@ public class OrderItemDTO {
 
     public OrderItemDTO(Long productId,
                         String name, Double price,
-                        Integer quantity) {
+                        Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -27,6 +29,7 @@ public class OrderItemDTO {
         this.name = entity.getProduct().getName();
         this.price = entity.getPrice();
         this.quantity = entity.getQuantity();
+        this.imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -63,5 +66,9 @@ public class OrderItemDTO {
 
     public Double getSubTotal(){
         return quantity * price;
+    }
+
+    public String getImgUrl(){
+        return imgUrl;
     }
 }
